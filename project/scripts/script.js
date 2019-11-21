1f05246e908bda8b9c3524eec6d8c29a0d268a63
Max Hallsett <max.hallsett@bee.io>
2019-11-21 15:43:00 -0500

    projects

--- project/scripts/script.js
+++ project/scripts/script.js
@@ -1,7 +1,8 @@
 const projectsButton = document.getElementById('projectsButton'),
-      body = document.getElementsByTagName('body'),
-      home_link = document.getElementById('home_link'),
+    body = document.getElementsByTagName('body'),
+    home_link = document.getElementById('home_link'),
     about_me = document.getElementById('about_me'),
+    projects = document.getElementById('projects'),
     main_heading = document.getElementById('main_heading');
 
 function addListeners() {
@@ -17,7 +18,7 @@ function initProjects() {
     setTimeout(function () {
         main_heading.innerHTML = 'My Projects';;
     }, 1750);
-
+    projects.classList.add('flyin_right');
 }
 
 
