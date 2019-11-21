9f13d856990f0f5a8fb03e2b29b74058c8cb5175
Max Hallsett <max.hallsett@bee.io>
2019-11-21 14:53:33 -0500

    h1 test change

--- project/scripts/script.js
+++ project/scripts/script.js
@@ -1,7 +1,8 @@
 const projectsButton = document.getElementById('projectsButton'),
       body = document.getElementsByTagName('body'),
       home_link = document.getElementById('home_link'),
-      about_me = document.getElementById('about_me');
+    about_me = document.getElementById('about_me'),
+    main_heading = document.getElementById('main_heading');
 
 function addListeners() {
     projectsButton.addEventListener('click', function () {
@@ -13,6 +14,7 @@ function initProjects() {
     body[0].classList.add('projects');
     body[0].classList.remove('home');
     about_me.classList.add('fadeout');
+    main_heading.innerHTML = 'My Projects';
 }
 
 
