7f048858ffdbf4398269c48a3822001167c56f8f
Max Hallsett <max.hallsett@bee.io>
2019-11-21 20:33:40 -0500

    templates

--- project/scripts/script.js
+++ project/scripts/script.js
@@ -1,30 +1,76 @@
-const projectsButton = document.getElementById('projectsButton'),
-    body = document.getElementsByTagName('body'),
+const body = document.getElementsByTagName('body'),
     home_link = document.getElementById('home_link'),
     about_me = document.getElementById('about_me'),
     projects = document.getElementById('projects'),
+    projectContainer = document.getElementById('projectContainer'),
     main_heading = document.getElementById('main_heading'),
-    project_li = document.querySelectorAll('#projects li');
+    project_li = document.querySelectorAll('#projects li'),
+    overlay = document.querySelector('overlay');
+
+//Project Templates
+const project = [
+    {
+        content: 'I was the Lead Engineer for a system which helped revolutionize the U.S. Navy\'s shipboard logging process. The system I led development on not only allows decision-makers to view near real-time ship analytics, but it also empowers them to act more agilely when it comes to decisions on energy consumption and management. Before this solution, the U.S. Navy maintained manual paper logging on all ships.' },
+        { heading: 'Beacon Interactive Systems' },
+        { image: '../images/beacon.png' }
+    ]
 
 function addListeners() {
-    projectsButton.addEventListener('click', function () {
-        initProjects();
+
+    document.addEventListener('click', function (e) {
+        if (e.target.id = 'projectsButton') {
+            initProjects();
+        } else if (e.target);
+
     });
 }
 
+function generateTemplate(project) {
+    const template = '<p>' + project.content + '</p> <img aria-hidden="true" alt="Project Image" src="' + project.image + '">';
+
+    return template;
+}
+
+function animateOverlay(color) {
+    if (color && overlay) {
+        overlay.style.background = color;
+    }
+    body[0].classList.add('animate');
+    body[0].classList.remove('animate');
+}
+
+function getOverlayColor(elm) {
+    return elm.style.color;
+}
+
+function updateProjectInfo(template) {
+    setTimeout(function () {
+        projectContainer.innerHTML = template.content;
+    }, 500);
+    setMainHeading(template.heading);
+}
+
+function setMainHeading(heading) {
+    setTimeout(function () {
+        main_heading.innerHTML = heading;
+    }, 1750);
+}
+
+
 function initProjects() {
     body[0].classList.add('projects');
-    body[0].classList.add('animate');
+    animateOverlay(color);
     about_me.classList.add('fadeout');
-    setTimeout(function () {
-        main_heading.innerHTML = 'My Projects';
-    }, 1750);
+
+    // template content and heading
+
     setTimeout(function () {
         about_me.style.display = "none";
     }, 500);
 
     let timeout = 0;
 
+    //animates project list
     for (let i = 0; i < project_li.length; i++) {
         project_li[i].style.display = "block";
         timeout += 250;
@@ -33,8 +79,6 @@ function initProjects() {
             project_li[i].classList.add('flyin_right');
         }, timeout);
     }
-
-    body[0].classList.remove('animate');
 
 }
 
