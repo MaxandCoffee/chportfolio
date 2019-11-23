388830a46ec59c096bca22be0d6f92335dfa9d16
Max Hallsett <max.hallsett@bee.io>
2019-11-22 22:11:31 -0500

    up

--- project/scripts/script.js
+++ project/scripts/script.js
@@ -143,7 +143,7 @@ function animateTriangles(color) {
         newBottomColor = color;
 
     topTriangle.style.borderColor = newTopColor + 'transparent transparent transparent';
-    bottomTriangle.style.borderLeftColor = newBottomColor;
+    bottomTriangle.style.borderColor = 'transparent transparent transparent' + newBottomColor;
 }
 
 function generateTemplate(arr) {
